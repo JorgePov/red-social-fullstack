02e9f8e7cfb6be214e5db5b0bf1d705bdde7b2dc
Jorge Poveda <jpoveda731@gmail.com>
2023-12-28 16:38:44 -0500

    feat: view register

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,41 +1,47 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
 })
 export default class LoginComponent {
-  fb = inject(FormBuilder)
-  loginService = inject(AuthService)
-  router = inject(Router)
-  loginform: FormGroup
+  fb = inject(FormBuilder);
+  loginService = inject(AuthService);
+  router = inject(Router);
+  loginform: FormGroup;
   constructor() {
     this.loginform = this.fb.group({
       email: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      password: ['', Validators.required],
+    });
   }
 
   login() {
-
-
     if (this.loginform.valid) {
-      const { email, password } = this.loginform.value
-      const body = { email, password }
+      const { email, password } = this.loginform.value;
+      const body = { email, password };
 
       this.loginService.login(body).subscribe({
         next: (data) => {
-          this.loginService.setUserData(data)
-          this.router.navigate(['/dashboard/messages'])
+          this.loginService.setUserData(data);
+          this.router.navigate(['/dashboard/messages']);
         },
-        error: (error) => { console.log(error) },
-        complete: () => { },
-      })
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {},
+      });
     }
   }
 }
